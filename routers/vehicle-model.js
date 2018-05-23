75f7b5e73be99d2839af5bdccc23f1d98b5d51a6
Emry Rosario <emry3008@gmail.com>
2018-05-23 14:44:54 -0400

    continuation of the router for vehicle models

--- routers/vehicle-model.js
+++ routers/vehicle-model.js
@@ -1,22 +1,88 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose');
-const vehicleModel = require('../models/vehicle-models')
+const VehicleModel = require('../models/vehicle-models')
 
 let dbConnection = process.env.DB_CONNECTION
 
 
-router.post('/vehicle-model', async (req, res) => {
+router.post('/vehicle-model', (req, res) => {
+    if (!req.body.vehicleModel || !req.body.vehicleModel.description) {
+        res.status(400)
+        console.log('POST /vehicle-model error: "Not Vehicle model description found."')
+        return res.json({error: 400, msg: 'Not Vehicle model description found.'})
+    }
       
     mongoose.connect(dbConnection)
-    let db = mongoose.connection
-
-    let vehicle = new vehicleModel()
-    vehicle.description = 'Accord'
-    vehicle.state = 'Activo'
-    await vehicle.save().catch((err) => console.log(err))
-    mongoose.disconnect()
-    res.json(vehicle)        
+    let model = req.body.vehicleModel
+    
+    let vModel = new VehicleModel({
+        description: model.description,
+        state: model.state || 'Activo',
+        employee: '5b05846f6ba18f3fc0a1da44'
+    })
+    vModel.save()
+    .then(model => {
+        mongoose.disconnect()
+        res.json(model)
+    })
+    .catch(err => {
+        res.status(500)
+        mongoose.disconnect()
+        res.json({message: err.message})
+    })        
 })
 
+router.get('/vehicle-model', (req, res) => {
+    mongoose.connect(dbConnection)
+    
+    VehicleModel.find()
+    .populate('employee')
+    .exec((err, models) => {
+        mongoose.disconnect();
+       if (err) {
+           res.status(500)
+           return res.send()
+       }
+
+       return res.json(models)
+    })
+})
+
+router.put ('/vehicle-model/:id', (req, res) => {
+    mongoose.connect(dbConnection)
+    let condition = {}
+    let update = req.body.update
+    
+    if (update.id || update._id) {
+        delete update.id
+        delete update._id
+    }
+
+    if (req.params.id) condition._id = req.params.id
+
+    VehicleModel.findOneAndUpdate(condition, update)
+    .exec()
+    .then(modelUpdated => {
+        mongoose.disconnect()
+        res.json(modelUpdated)
+    })
+    .catch(err => {
+        mongoose.disconnect()
+        res.status(500)
+        res.json({error: 'Error'})
+    })
+})
+
+router.delete('/vehicle-model/:id', (req, res) => {
+    mongoose.connect(dbConnection)    
+
+    let condition = {}
+    let toDelete = req.body.delete;
+
+    if (req.params.id) condition._id = req.params.id
+
+    vehicleModel.find(condition)
+    //TODO
+})
 module.exports = router
