dfcd2de0423ff5c9c260957cf5281d85aac02876
Emry Rosario <emry3008@gmail.com>
2018-05-28 00:26:52 -0400

    added authentication with passport using local strategy

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 const app = express()
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 const Vehicle = require('./models/vehicles')
 const VehicleType = require('./models/vehicle-types')
@@ -23,10 +25,20 @@ const rentRouter = require('./routers/rent')
 const customerRouter = require('./routers/customer')
 const transactionRouter = require('./routers/transaction')
 const inspectionRouter = require('./routers/inspection')
+const expressSession = require('express-session')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
+
+app.use(expressSession({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 let dbConnection = process.env.DB_CONNECTION
 
@@ -39,6 +51,54 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('We\'re connected...')
 
+})
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    Employee.findOne({ user: username, password },  (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      user.set('password', null)
+      return done(null, user);
+    })
+  }
+))
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+
+})
+
+passport.deserializeUser(function(id, done) {
+  Employee.findById(id)
+  .then(user => {
+    user.set('password', null)
+    done(null, user)
+  })
+  .catch(err => done(err, false))
+})
+
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    // req.user is available for use here
+    return next(); }
+
+  // denied. redirect to login
+  res.redirect('/login')
+}
+
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/login', (req,res) => {
+  res.json({authenticationStatus: false})
+})
+
+app.get('/', ensureAuthenticated, (req, res) => {
+  res.json(req.user)
 })
 
 app.use('/', vehicleModelRouter)
